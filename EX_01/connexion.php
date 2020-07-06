fc50b318fe4a41815372733f3dcf58a8f522ce5f
Camille85 <67278268+Camille85@users.noreply.github.com>
2020-07-06 17:46:35 +0200

    Jour 09

---
+++ EX_01/connexion.php
@@ -0,0 +1,17 @@
+<html>
+<form action="securite.php" method="post" enctype= "multipart/form-data">
+            <div>
+                <label for="login"> </label>
+                <input type="text" id="login" name="user_login" placeholder="Login">
+            </div>
+            <div >
+                <label for="password"></label>
+                <input type="text" id="password" name="user_password" placeholder="Password">
+            </div>
+            
+            <div >
+                <button type="submit" id="button">Connexion</button>
+            </div>
+        
+        </form>
+</html>
